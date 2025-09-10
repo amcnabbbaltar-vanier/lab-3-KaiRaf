0a1c4b3bb128e4e019ba7c68ad7d139056d61a25
Kais Rafie <2371100@edu.vaniercollege.qc.ca>
2025-09-10 11:33:39 -0400

    Complete task 2

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class GunComponent : MonoBehaviour
@@ -9,5 +10,39 @@ public class GunComponent : MonoBehaviour
     private float chargeTime = 0.0f;
     private bool isCharging = false;
 
-   
+    void Start()
+    {
+        chargeTime = Mathf.Clamp(chargeTime, 0, maxChargeTime);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            chargeTime = 0.0f;
+            isCharging = true;
+        }
+        if (isCharging)
+        {
+            chargeTime += Time.deltaTime;
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ShootBullet();
+            isCharging = false;
+        }
+    }
+
+
+    void ShootBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position,
+                            bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        float bulletImpulse = bulletMaxImpulse * chargeTime;
+
+        rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+
+    }
+
 }
